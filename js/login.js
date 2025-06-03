c73e36f334e3b38b5d13d4ae8f0d76438a8a6436
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-06-03 19:05:53 -0300

    commit

--- js/login.js
+++ js/login.js
@@ -1,1 +1,13 @@
 // autenticacao login
+const botao = document.getElementById('entrar');
+botao.addEventListener('click',
+    function (){
+        const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+        let login = document.getElementById("login").value;
+        let senha = document.getElementById("senha").value;
+
+        const logado = usuarios.find(user => user.login === login && user.senha === senha);
+
+
+    }
+);
